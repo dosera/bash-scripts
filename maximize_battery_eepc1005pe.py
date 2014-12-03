422a1a8bd051db5d159b44fc139c9e96a1f87ed9
André Doser <doser.andre@gmail.com>
2014-12-03 16:55:28 +0100

    Added description. Refactoring.

--- maximize_battery_eepc1005pe.py
+++ maximize_battery_eepc1005pe.py
@@ -11,54 +11,78 @@ import sys
 from optparse import OptionParser
 
 
-def call_modprobe_funcion(status, *args):
-    for arg in args:
-        system('sudo modprobe {} {}'.format('-r' if status else '', arg))
+class SaveBattery:
+    def __init__(self):
+        self._audio_driver = 'snd_hda_intel'
+        self._audio_codec = 'snd_hda_codec_realtek'
+        self._wifi_driver = 'ath9k'
 
+    def call_modprobe_function(self, status, *args):
+        """
+        Loads/Unloads the given drivers.
 
-# unload audio driver (snd_hda_*)
-def audio_func(b):
-    call_modprobe_funcion(b, 'snd_hda_intel', 'snd_hda_codec_realtek')
+        :param status: Load or unload
+        :param args: The drivers
+        """
+        for arg in args:
+            system('sudo modprobe {} {}'.format('-r' if status else '', arg))
 
+    def audio_func(self, b):
+        """
+        Load/Unload the audio driver
 
-# unload the WIFI driver (ath9k) and stop wicd
-def wifi_func(b):
-    call_modprobe_funcion(b, 'ath9k')
-    if b:
-        system('sudo systemctl stop wicd')
-        system('killall wicd-client')
-    else:
-        system('sudo systemctl start wicd')
-        print("Please start wicd-client to connect to a WIFI network now.")
+        :param b: Load or unload
+        """
+        self.call_modprobe_function(b, self._audio_driver, self._audio_codec)
 
+    def wifi_func(self, b):
+        """
+        Load/Unload the wifi driver
 
-def get_driver_status(*args):
-    result = []
-    for arg in args:
-        result.append(True if popen('lsmod | grep {}'.format(arg))
-                      .readlines() else False)
-    # powerup or down?
-    watch_f = '/proc/sys/kernel/nmi_watchdog'
-    result.append('Up' if '1' in open(watch_f, 'r').read() else 'Down')
-    return tuple(result)
+        :param b: Load or unload
+        """
+        self.call_modprobe_funcion(b, self._wifi_driver)
+        if b:
+            system('sudo systemctl stop wicd')
+            system('killall wicd-client')
+        else:
+            system('sudo systemctl start wicd')
+            print("Please start wicd-client to connect to a WIFI network now.")
 
+    def get_driver_status(self, *args):
+        """
+        Get the status of the drivers and the powerdown script.
 
-def powerup(watch_status):
-    """
-    Call the power{down,up} script
-    (https://wiki.archlinux.org/index.php/Powerdown)
+        :param args: Drivers to check
 
-    :param watch_status: Status of powerdown;
-                         'Up' if powerup, 'Down' if powerdown is active
-    """
-    if 'Up' in watch_status:
-        system('sudo /usr/bin/powerdown')
-    else:
-        system('sudo /usr/bin/powerup')
+        :returns: tuple containg driver status for each driver
+                  as well as the status of the powerdown script
+        """
+        result = []
+        for arg in args:
+            result.append(True if popen('lsmod | grep {}'.format(arg))
+                          .readlines() else False)
+        watch_f = '/proc/sys/kernel/nmi_watchdog'
+        result.append('Up' if '1' in open(watch_f, 'r').read() else 'Down')
+        return tuple(result)
+
+    def powerup(self, watch_status):
+        """
+        Call the power{down,up} script
+        (https://wiki.archlinux.org/index.php/Powerdown)
+
+        :param watch_status: Status of powerdown;
+                             'Up' if powerup, 'Down' if powerdown is active
+        """
+        if 'Up' in watch_status:
+            system('sudo /usr/bin/powerdown')
+        else:
+            system('sudo /usr/bin/powerup')
 
 if __name__ == '__main__':
+    sb = SaveBattery()
     # get the status of the drivers
-    driver_status_list = get_driver_status('ath9k', 'snd_hda_')
+    driver_status_list = sb.get_driver_status('ath9k', 'snd_hda_')
     # initialize the OptionParser
     parser = OptionParser(epilog="Wifi: {}, Audio: {}, Power: {}"
                           .format(*driver_status_list))
@@ -78,10 +102,10 @@ if __name__ == '__main__':
         sys.exit(1)
     # wifi
     if opts.wifi:
-        wifi_func(driver_status_list[0])
+        sb.wifi_func(driver_status_list[0])
     # audio
     if opts.audio:
-        audio_func(driver_status_list[1])
+        sb.audio_func(driver_status_list[1])
     # power
     if opts.power:
-        powerup(driver_status_list[2])
+        sb.powerup(driver_status_list[2])
